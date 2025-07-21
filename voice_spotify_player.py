b611239b11a26cfb39ce40649a6dd9dc243522ee
Leon Nguyen <leonnguyen0916@gmail.com>
2025-07-20 19:10:59 -0700

    changed id and secret

--- voice_spotify_player.py
+++ voice_spotify_player.py
@@ -3,8 +3,8 @@ from spotipy.oauth2 import SpotifyOAuth
 import speech_recognition as sr
 
 # Replace these with your actual values
-SPOTIPY_CLIENT_ID = 'your-client-id'
-SPOTIPY_CLIENT_SECRET = 'your-client-secret'
+SPOTIPY_CLIENT_ID = '32fa50f4c0154802af23c5af0c5e2a90'
+SPOTIPY_CLIENT_SECRET = 'cacce2027b3149b59f4fc3f7313b66d9'
 SPOTIPY_REDIRECT_URI = 'http://localhost:8888/callback'
 
 # Set up Spotify authorization
